9a48448b625f2c17b9a1387ef7d598a1b952aac8
BedratyiDV <bedrik2023@gmail.com>
2023-12-07 09:58:59 +0100

    The First commit
    07/12/2023

---
+++ SerializationExercise/Person.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace SerializationExercise
+{
+    [Serializable]
+    internal class Person
+    {
+        [JsonProperty("personFirstName")]
+        private string _firstName;
+
+        [JsonProperty("personLastName")]
+        private string _lastName;
+
+        [JsonProperty("personAge")]
+        private int _age;
+
+        [NonSerialized]
+        private int _weight;
+
+        public Person(string firstName, string lastName, int age, int weight)
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+            _age = age;
+            _weight = weight;
+        }
+        public string FirstName
+        {
+            get => _firstName;
+        }
+        public string LastName
+        {
+            get => _lastName;
+        }
+        public int Age
+        {
+            get => _age;
+        }
+        public int Weight
+        {
+            get => _weight;
+        }
+    }
+}
---
+++ SerializationExercise/Program.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+
+namespace SerializationExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creating an instance of Person object
+            Person personDorian = new Person("Dorian","Grey", 43, 80);
+
+            //SerializationMethod defining
+            void SerializationMethod(Person person) 
+            
+            {
+                //Serialization into JSON
+                var serialized = JsonConvert.SerializeObject(person);
+                Console.WriteLine(serialized);
+
+                //Serialization into binary format
+                IFormatter formatter = new BinaryFormatter();
+                byte[] serializedData;
+                using (MemoryStream stream = new MemoryStream()) 
+                
+                { 
+                    formatter.Serialize(stream, serialized);
+                    serializedData = stream.ToArray();
+                    Console.WriteLine("Serialized Data:");
+                    Console.WriteLine(BitConverter.ToString(serializedData).Replace("-", " "));
+                    
+                }
+                //Deserialization from binary to JSON
+                using (MemoryStream stream = new MemoryStream(serializedData))
+                {
+                    string jsonString = formatter.Deserialize(stream).ToString();
+                    Console.WriteLine();
+                    Console.WriteLine(jsonString);
+
+                    //Deserialization from JSON into Person object
+                    Person deserializedPerson = JsonConvert.DeserializeObject<Person>(jsonString);
+                    
+                    Console.WriteLine("Deserialized Person First name:");
+                    Console.WriteLine(deserializedPerson.FirstName);
+                    Console.WriteLine("Deserialized Person Last name:");
+                    Console.WriteLine(deserializedPerson.LastName);
+                    Console.WriteLine("Deserialized Person Age:");
+                    Console.WriteLine(deserializedPerson.Age);
+                    Console.WriteLine("Deserialized Person Weight:");
+                    Console.WriteLine(deserializedPerson.Weight);
+
+                }
+            }
+            //Call of SerializationMethod 
+            SerializationMethod(personDorian);
+        }
+    }
+}
